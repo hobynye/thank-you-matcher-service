31a47b44250613136d1408e1fc8fbcc83bf3f685
Tim Walshjamin <walshta@users.noreply.github.com>
2026-01-15 18:32:54 -0500

    Add integration test

--- src/test/java/org/hobynye/tym/ApplicationIT.java
+++ src/test/java/org/hobynye/tym/ApplicationIT.java
@@ -8,12 +8,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest
-class ApplicationTest {
+public class ApplicationIT {
+
     ApplicationContext context;
 
     @Test
     void contextLoads() {
         assertThat(context, notNullValue());
-
     }
 }
